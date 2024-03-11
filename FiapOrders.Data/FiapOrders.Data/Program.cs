d36081cc0532ba0c6dd9bbad52d3ac0be16b3c1a
Maurici Alvarez <maurici.alvarez@gmail.com>
2024-03-10 22:05:22 -0300

    Add logging for FiaOrdersData

--- FiapOrders.Data/FiapOrders.Data/Program.cs
+++ FiapOrders.Data/FiapOrders.Data/Program.cs
@@ -2,8 +2,29 @@ using FiapOrders.Core.Domain.Configurations;
 using FiapOrders.Data.Configuration;
 using FiapOrders.Data.Events;
 using MassTransit;
+using Serilog;
+using System.Reflection;
 
 var builder = Host.CreateApplicationBuilder(args);
+
+var sqlUrl = Environment.GetEnvironmentVariable("DatalustSeq.Url");
+
+builder.Logging.ClearProviders();
+var logger = new LoggerConfiguration()
+    .MinimumLevel.Information()
+    .Enrich.WithProperty("Version", Assembly.GetEntryAssembly()!.GetName().Version)
+    .Enrich.WithEnvironmentName()
+    .Enrich.WithMachineName()
+    .Enrich.WithProcessId()
+    .Enrich.WithThreadId()
+    .Enrich.WithMemoryUsage()
+    .Enrich.FromLogContext()
+    .WriteTo.Console()
+    .WriteTo.Seq(sqlUrl)
+    .CreateLogger();
+builder.Logging.AddSerilog(logger);
+
+
 builder.Services.AddRepositories();
 builder.Services.AddServices();
 
